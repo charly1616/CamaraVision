dd36039e6bceb7104b1e4a7e0b50ff612ddbffa0
charly1616 <124099579+charly1616@users.noreply.github.com>
2023-09-07 21:41:25 -0500

    tuk

--- src/Model/Camara.java
+++ src/Model/Camara.java
@@ -5,13 +5,14 @@ import javax.swing.JFrame;
 import org.opencv.core.Core;
 public class Camara extends JFrame{
     public Camara(){
+        this.setSize(new Dimension(600,600));
         setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
     
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("Exitooooo");
         
     }
     
